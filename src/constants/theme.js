a17a84ebbe1b9d7a927631af59a9ac514050045e
Miles <spacemonkeycode@gmail.com>
2026-05-15 18:24:52 -0600

    Initial commit

---
+++ src/constants/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+  bg: '#020617',
+  primary: '#38bdf8',
+  text: '#f8fafc',
+  subtext: '#94a3b8',
+  glass: 'rgba(255, 255, 255, 0.03)',
+  glassBorder: 'rgba(255, 255, 255, 0.1)',
+};
